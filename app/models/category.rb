3d47e61f29dbee5ecb0dd4a34e85f768eee5d9f7
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-07-24 15:47:12 -0500

    Table still needs works.  Category names appear correctly

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :dishes
+
+  def display_name
+    if self.name.include?("_")
+      self.name.split("_").join(" ").titleize
+    else
+      self.name.titleize
+    end
+  end
 end
