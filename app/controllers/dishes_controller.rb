28592711aad0466ff07c0130a551fcb87b6d1f85
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-07-26 22:40:01 -0500

    Able to add new dishes. Need to clean up some bugs

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,44 +4,44 @@ class DishesController < ApplicationController
     @dishes = Dish.all
     @menu_dish = current_order.menu_dishes.new
   end
-  #
-  # def create
-  #   @dish = Dish.new(dish_params)
-  #   if @dish.save
-  #     redirect_to dishes_path
-  #   else
-  #     render "new"
-  #   end
-  # end
-  #
-  # def new
-  #   @dish = Dish.new
-  # end
-  #
-  # def show
-  #   @dish = Dish.find(:id)
-  # end
-  #
-  # def edit
-  #   @dish = Dish.find(:id)
-  # end
-  #
-  # def update
-  #   @dish = Dish.find(:id)
-  #   if @dish.update_attributes(dish_params)
-  #     redirect_to dishes_path
-  #   else
-  #     render "edit"
-  #   end
-  # end
-  #
-  # def destroy
-  #   @dish = Dish.find(:id).destroy
-  #   redirect_to dishes_path
-  # end
-  
+
+  def create
+    @dish = Dish.new(dish_params)
+    if @dish.save
+      redirect_to dishes_path
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @dish = Dish.new
+  end
+
+  def show
+    @dish = Dish.find(:id)
+  end
+
+  def edit
+    @dish = Dish.find(:id)
+  end
+
+  def update
+    @dish = Dish.find(:id)
+    if @dish.update_attributes(dish_params)
+      redirect_to dishes_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @dish = Dish.find(:id).destroy
+    redirect_to dishes_path
+  end
+
   private
-  def document_params
+  def dish_params
     params.require(:dish).permit(:name, :price, :category_id, :description)
   end
 
