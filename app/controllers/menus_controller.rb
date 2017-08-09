0803ffcb8ef639598e454ca09245b690d85d0858
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-08-09 18:48:55 -0500

    Cart works again.  working on sessions and default ordering

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,4 +2,8 @@ class MenusController < ApplicationController
   def show
     @menu_dishes = current_order.menu_dishes
   end
+  # def show
+  #   @menu = Menu.find(session[:menu_id])
+  #   @menu_dishes = @menu.menu_dishes.order(dish_id.category_id)
+  # end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,9 @@
 class Menu < ApplicationRecord
-  belongs_to :order_status
+  # belongs_to :order_status
   has_many :menu_dishes
-  before_validation :set_order_status, on: :create
+  # before_validation :set_order_status, on: :create
   before_save :update_subtotal
+  scope :ordered, -> { joins(:dishes).order('dish.category_id')}
 
   def subtotal
     menu_dishes.collect { |d| d.valid? ? (d.quantity * d.unit_price) : 0 }.sum
