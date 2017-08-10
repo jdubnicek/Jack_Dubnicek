b73e6bb11de27ff7206b7f8767442f01fb69fd3b
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-08-09 22:45:57 -0500

    Install gem and dependencies

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/convert"
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
